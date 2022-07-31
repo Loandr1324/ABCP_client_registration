9f745a75e6284b445a79d8f9567b90c87240c47d
Loik Andrey <7034@balancedv.ru>
2022-07-31 14:44:21 +1100

    Создал файлы для запуска на Heroku

---
+++ config.py
@@ -0,0 +1,9 @@
+
+USERNAME = 'api@id33871'
+MD5PASS = '13e4fe7a6c0b907bffcae6a10a67e672'
+
+profileSV_ID = 7072696 # ID профиля Розница ШСВ 5%
+profileAV_ID = 7072698 # ID профиль Розница ШАВ 5%
+
+GET_URL_USERS = 'https://id33871.public.api.abcp.ru/cp/users?'
+POST_URL_USER = 'https://id33871.public.api.abcp.ru/cp/user'
